3e7213d4d96e1c75ec8d4ab738f5ba5949b9dcdd
pushpendra0230 <153480201+pushpendra0230@users.noreply.github.com>
2024-12-17 14:29:23 +0530

    day02

--- Day01/day01/src/App.jsx
+++ Day01/day01/src/App.jsx
@@ -4,11 +4,9 @@ const App=()=> {
   return (
     <div>
         <h1 className="text-3xl font-bold underline">
-      pushpendra soni 
+      pushpendra soni
     </h1>
     </div>
   )
 }
-
 export default App
-
---
+++ day2/day02/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CardComponent from "./components/CardComponent";
+import products from "./utility/data";
+
+const App = () => {
+  return (
+    <div className="flex flex-wrap">
+      <CardComponent contacts={products} />
+    </div>
+  );
+};
+export default App;
