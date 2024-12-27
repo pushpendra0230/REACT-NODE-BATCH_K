bef29d3094335879dce25de066a7df3a9371d72a
pushpendra0230 <153480201+pushpendra0230@users.noreply.github.com>
2024-12-27 15:34:13 +0530

    Deleted day5/my-project files and folders

--- day5/my-project/src/assets/utility/script.js
+++
@@ -1,8 +0,0 @@
-import products from "./data.js";
-console.log(products);
-// products.map((product) => console.log(product));
-const p = products.map((product) => ({
-  category: product.category,
-  item: product.productName,
-}));
-console.log(p);
