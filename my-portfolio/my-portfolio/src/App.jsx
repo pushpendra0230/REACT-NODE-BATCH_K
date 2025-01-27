14a5eb9c9bdb6de52c7d795f8b43a3ce3aa01fc0
pushpendra0230 <153480201+pushpendra0230@users.noreply.github.com>
2025-01-27 20:41:42 +0530

    My Portfolio

---
+++ my-portfolio/my-portfolio/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CardComponent from "./components/CardComponent";
+
+const App = () => {
+  return (
+    <div>
+      <CardComponent />
+    </div>
+  );
+};
+export default App;
